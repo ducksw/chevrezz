7474936a30cf0f993746e933369cb3700619803a
duksito <cansgamer04@gmail.com>
2024-06-11 18:32:03 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,25 +1,7 @@
 var code = document.getElementById('code');
-function token(l) {
-  let save = ""; 
-  let token = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  let tokenLength = token.length;
-
-  for (let i = 0; i < l; i++) {
-    save += token.charAt(Math.random() * tokenLength);
-  }
-
-  return save;
-}
-
-var t = token(50)
-code.innerHTML = `<b>${t}</b>` 
+const random = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+code.innerHTML = random.toUpperCase();
 
 function direct() {
-  /*
-  let promp = prompt("Ingrese el Codigo");
-  if (promp == t) {
-    window.open("game.html", "_self");
-  }
-  */
-  alert("Esta funcionalidad no esta activa");
+  alert("AUN NO ES FUNCIONAL");
 }
