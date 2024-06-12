6e5722c54a75705b9dc326defcc2e52c10120507
duksito <cansgamer04@gmail.com>
2024-06-11 22:11:51 -0500

    test function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
+/*
 var code = document.getElementById('code');
 const random = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
 code.innerHTML = random.toUpperCase();
+*/
 
 function direct() {
   alert("AUN NO ES FUNCIONAL");
 }
+
